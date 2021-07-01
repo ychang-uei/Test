93260ca18da29518574cd9c3e43efa9db235e439
ychang@uei.com <ychang@uei.com>
2021-07-01 11:31:01 -0700

    First commit

---
+++ Test/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void listView_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            listView.IsVisible = false;
+        }
+
+        void listView_ItemTapped(System.Object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            listView.IsVisible = false;
+        }
+    }
+}
